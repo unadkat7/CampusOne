6f0fd921d7b1d825b25adf9a68afa31b395ee706
unadkat7 <153261041+unadkat7@users.noreply.github.com>
2026-03-27 19:40:11 +0530

    chore - main update

--- backend/routes/api/mySubjects.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const subjectControllers = require('../../controllers/subject.controller');
-const auth = require('../../middleware/auth');
-
-router.get('/', auth, subjectControllers.getMySubjects);
-
-module.exports = router;
